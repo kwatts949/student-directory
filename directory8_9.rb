a3a5cc48a42477108208492b3fddca44438ef80a
Kayleigh Watts <kwatts949@gmail.com>
2022-10-04 13:53:45 +0100

    Exercise 8.9

---
+++ directory8_9.rb
@@ -0,0 +1,49 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    # prints correctly if only 1 student
+    if students.length ==1
+      puts "Now we have #{students.count} student"
+    else
+      puts "Now we have #{students.count} students"
+    end
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(students)
+  # prints correctly for 1 student
+    if students.length == 1
+      puts "Overall, we have #{students.count} student"
+    else 
+      puts "Overall, we have #{students.count} great students"
+    end
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
+
